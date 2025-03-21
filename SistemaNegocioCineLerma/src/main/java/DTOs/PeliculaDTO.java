d08dfc41855df554903f63e240cb64020f0230e8
DanielMR0509 <daniel.miramontes252801@potros.itson.edu.mx>
2025-03-20 21:48:20 -0700

    Implementada clase de PeliculaDTO
    Se implementó la clase de PeliculaDTO. Detalle en el atributo "peliculaImagen", debe ser de tipo Imagen. Por el momento será de tipo String.

--- SistemaNegocioCineLerma/src/main/java/DTOs/PeliculaDTO.java
+++ SistemaNegocioCineLerma/src/main/java/DTOs/PeliculaDTO.java
@@ -6,8 +6,45 @@ package DTOs;
 
 /**
  *
- * @author Ramon Valencia
+ * @author Daniel Miribe
  */
 public class PeliculaDTO {
-    
+
+    private String nombrePelicula;
+    private String peliculaImagen; // ESTE DEBE SER DE TIPO IMAGEN
+    public String descripcionPelicula;
+
+    public PeliculaDTO() {
+    }
+
+    public PeliculaDTO(String nombrePelicula, String peliculaImagen, String descripcionPelicula) {
+        this.nombrePelicula = nombrePelicula;
+        this.peliculaImagen = peliculaImagen;
+        this.descripcionPelicula = descripcionPelicula;
+    }
+
+    public String getNombrePelicula() {
+        return nombrePelicula;
+    }
+
+    public void setNombrePelicula(String nombrePelicula) {
+        this.nombrePelicula = nombrePelicula;
+    }
+
+    public String getPeliculaImagen() {
+        return peliculaImagen;
+    }
+
+    public void setPeliculaImagen(String peliculaImagen) {
+        this.peliculaImagen = peliculaImagen;
+    }
+
+    public String getDescripcionPelicula() {
+        return descripcionPelicula;
+    }
+
+    public void setDescripcionPelicula(String descripcionPelicula) {
+        this.descripcionPelicula = descripcionPelicula;
+    }
+
 }
