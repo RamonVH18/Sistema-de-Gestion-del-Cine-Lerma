08d1b70ae2169fff85b8e1edeceebb943cad3357
Isaac <isaacgro80@gmail.com>
2025-03-21 01:05:52 -0700

    DTOs
    creacion de BoletoDTO y PagoDTO

--- SistemaNegocioCineLerma/src/main/java/DTOs/BoletoDTO.java
+++ SistemaNegocioCineLerma/src/main/java/DTOs/BoletoDTO.java
@@ -4,10 +4,77 @@
  */
 package DTOs;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author Ramon Valencia
  */
 public class BoletoDTO {
     
+    private String nombrePelicula;
+    
+    private String imagenPelicula; // LUEGO LO CAMBIAMOS A IMAGEN
+    
+    private Date fechaHoraFuncion;
+    
+    private String numeroSala;
+    
+    private List<String> listaAsientosSeleccionados;
+
+    public BoletoDTO() {
+    }
+
+    public String getNombrePelicula() {
+        return nombrePelicula;
+    }
+
+    public BoletoDTO(String nombrePelicula, String imagenPelicula, Date fechaHoraFuncion, String numeroSala, List<String> listaAsientosSeleccionados) {
+        this.nombrePelicula = nombrePelicula;
+        this.imagenPelicula = imagenPelicula;
+        this.fechaHoraFuncion = fechaHoraFuncion;
+        this.numeroSala = numeroSala;
+        this.listaAsientosSeleccionados = listaAsientosSeleccionados;
+    }
+
+    public void setNombrePelicula(String nombrePelicula) {
+        this.nombrePelicula = nombrePelicula;
+    }
+
+    public String getImagenPelicula() {
+        return imagenPelicula;
+    }
+
+    public void setImagenPelicula(String imagenPelicula) {
+        this.imagenPelicula = imagenPelicula;
+    }
+
+    public Date getFechaHoraFuncion() {
+        return fechaHoraFuncion;
+    }
+
+    public void setFechaHoraFuncion(Date fechaHoraFuncion) {
+        this.fechaHoraFuncion = fechaHoraFuncion;
+    }
+
+    public String getNumeroSala() {
+        return numeroSala;
+    }
+
+    public void setNumeroSala(String numeroSala) {
+        this.numeroSala = numeroSala;
+    }
+
+    public List<String> getListaAsientosSeleccionados() {
+        return listaAsientosSeleccionados;
+    }
+
+    public void setListaAsientosSeleccionados(List<String> listaAsientosSeleccionados) {
+        this.listaAsientosSeleccionados = listaAsientosSeleccionados;
+    }
+    
+    
+    
+
 }
--- SistemaNegocioCineLerma/src/main/java/DTOs/PagoDTO.java
+++ SistemaNegocioCineLerma/src/main/java/DTOs/PagoDTO.java
@@ -4,10 +4,51 @@
  */
 package DTOs;
 
+import java.util.Date;
+
 /**
  *
  * @author Ramon Valencia
  */
 public class PagoDTO {
     
+  private Double monto;
+    
+    private Date fechaHora;
+    
+    private String estado;
+
+    public PagoDTO() {
+    }
+    
+
+    public PagoDTO(Double monto, Date fechaHora, String estado) {
+        this.monto = monto;
+        this.fechaHora = fechaHora;
+        this.estado = estado;
+    }
+
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    public Date getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(Date fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
