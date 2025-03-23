b4c032b7cc9f9c796e1a8a04b775996d8bc81b2a
Yizbin <abrahamcbringas@gmail.com>
2025-03-23 15:18:40 -0600

    Creacion de paypalDTO
    Se creo el paypalDTO

---
+++ SistemaTransferenciaCineLerma/src/main/java/DTOs/PaypalDTO.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTOs;
+
+/**
+ *
+ * @author Abraham Coronel Bringas
+ */
+public class PaypalDTO {
+
+    private String correo;
+
+    private String contrasenia;
+
+    public PaypalDTO() {
+    }
+
+    public PaypalDTO(String correo, String contrasenia) {
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+}
