279bcb3d9a197b24bea7fdeb22a60846bcfd843f
RamonVH18 <ramon.valencia253272@potros.itson.edu.mx>
2025-04-14 12:24:44 -0700

    Creacion de Excepciones, correcion
    Isaac...

---
+++ SistemaGestionReservaBoletos/src/main/java/Excepciones/ReservarAsientoFuncionException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excepciones;
+
+/**
+ *
+ * @author Ramon Valencia
+ */
+public class ReservarAsientoFuncionException extends Exception {
+
+    public ReservarAsientoFuncionException(String message) {
+        super(message);
+    }
+
+    public ReservarAsientoFuncionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
