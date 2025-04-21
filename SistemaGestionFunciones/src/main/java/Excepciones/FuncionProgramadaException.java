fbdcfab330dd8b59454967b1d12c3dd869de2828
Yizbin <abrahamcbringas@gmail.com>
2025-04-21 17:18:06 -0600

    Creacion CU Funciones
    Se crearon las pantallas de funciones y el caso de uso individual, falta agregar cosas.

---
+++ SistemaGestionFunciones/src/main/java/Excepciones/FuncionProgramadaException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Excepciones;
+
+/**
+ *
+ * @author Abraham Coronel Bringas
+ */
+public class FuncionProgramadaException extends Exception {
+
+    public FuncionProgramadaException(String message) {
+        super(message);
+    }
+
+    public FuncionProgramadaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ SistemaGestionFunciones/src/main/java/GestionFunciones/ManejoFunciones.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GestionFunciones;
+
+/**
+ *
+ * @author Abraham Coronel Bringas
+ */
+public class ManejoFunciones implements IManejoFunciones {
+    
+}
