bf5e0e116cb9fe169029c8fdff60fc2812ef0085
SEHUZ <sonic15622@gmail.com>
2025-05-22 00:26:58 -0700

    commit secreto...

--- SistemaPresentacionCineLerma/src/main/java/pantallas/IniciarSesion.java
+++ SistemaPresentacionCineLerma/src/main/java/pantallas/IniciarSesion.java
@@ -14,7 +14,7 @@ import enums.Rol;
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JOptionPane;
-//import wep.ifuknow;
+import wep.ifuknow;
 
 /**
  *
@@ -61,14 +61,14 @@ public class IniciarSesion extends javax.swing.JFrame {
 
                 addMouseListener(new java.awt.event.MouseAdapter() {
                     public void mouseClicked(java.awt.event.MouseEvent evt) {
-                        //  javax.swing.JFrame parentFrame = (javax.swing.JFrame) javax.swing.SwingUtilities.getWindowAncestor(jPanel2);
-                        //  ifuknow.showSplash(
-                            //      parentFrame,
-                            //      "/burro.png",
-                            //      "/sonido.wav",
-                            //     2000
-                            //  );
-                        // jPanel2.setVisible(false);
+                        javax.swing.JFrame parentFrame = (javax.swing.JFrame) javax.swing.SwingUtilities.getWindowAncestor(jPanel2);
+                        ifuknow.showSplash(
+                            parentFrame,
+                            "/burro.png",
+                            "/sonido.wav",
+                            2000
+                        );
+                        jPanel2.setVisible(false);
                     }
                 });
             }
