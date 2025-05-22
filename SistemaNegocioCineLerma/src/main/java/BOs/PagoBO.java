eda4d54a08b09dd49ae8b0837287bf5fa166121e
SEHUZ <sonic15622@gmail.com>
2025-05-22 16:33:51 -0700

    so sorry

--- SistemaNegocioCineLerma/src/main/java/BOs/PagoBO.java
+++ SistemaNegocioCineLerma/src/main/java/BOs/PagoBO.java
@@ -39,25 +39,6 @@ public class PagoBO {
     }
 
     public Double calcularMontoTotal(Integer boletos, FuncionDTO funcion) throws PagoValidacionException {
-        // Cantidad de boletos
-        if (boletos == null || boletos <= 0) {
-            throw new PagoValidacionException("La cantidad de boletos debe ser mayor a cero");
-        }
-
-        // validar que la funcion no sea nula
-        if (funcion == null) {
-            throw new PagoValidacionException("La funcion no puede ser nula");
-        }
-
-        // validar el precio de la funcion, aunque siempre deberia ser valido
-        if (funcion.getPrecio() == 0.0 || funcion.getPrecio() <= 0.0) {
-            throw new PagoValidacionException("Precio de función invalido");
-        }
-
-        // Validación 5: Fecha de función válida
-        if (funcion.getFechaHora().isBefore(LocalDateTime.now())) {
-            throw new PagoValidacionException("La funcion es pasada");
-        }
 
         Funcion funcionEntidad = funcionMapper.toFuncionEntidad(funcion);
 
@@ -66,26 +47,12 @@ public class PagoBO {
         return montoTotal;
     }
 
-    public Pago registrarPagoExitoso(PagoDTO pago) throws PagoValidacionException {
-        // Validar si el pago no es nulo
-        if (pago == null) {
-            throw new PagoValidacionException("El pago no puede ser nulo");
-        }
-
-        // Validar que el monto del pago no sea 0, menor o nulo
-        if (pago.getMonto() == null || pago.getMonto() <= 0) {
-            throw new PagoValidacionException("Monto de pago invalido");
-        }
-
-        // validar que la fecha no sea futura, que nunca deberia serlo
-        if (pago.getFechaHora() != null && pago.getFechaHora().isAfter(LocalDateTime.now())) {
-            throw new PagoValidacionException("Fecha de pago inválida");
-        }
+    public PagoDTO registrarPagoExitoso(PagoDTO pago) throws PagoValidacionException {
 
         Pago pagoRegistrar = PagoMapper.toPagoEntidad(pago);
         Pago pagoRegistrado = pagoDAO.registrarPagoExitoso(pagoRegistrar);
 
-        return pagoRegistrado;
+        return PagoMapper.toPagoDTO(pagoRegistrado);
     }
 
 }
